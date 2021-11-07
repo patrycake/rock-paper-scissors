a74b1c96bffcf19ea86f100e817d23215ace54aa
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-06 21:39:32 -0400

    added gameResult function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,9 +12,37 @@ function userPlay(){
     if(userchoice == "scissors"){return 3;}
 }
 
+function gamePlay(computerSelection, playerSelection){
+    let game = computerSelection + playerSelection;
+    // rock 1 paper 2 = 3 (paper wins)
+    // rock 1 scissors 3 = 4 (rock wins)
+    // paper 2 scissors 3 = 5 (scissors wins)
+    if (computerSelection == playerSelection) return {
+        playerWin: true,
+        computerWin: true,
+        message: `Its a Tie, both selected ${intToString(computerSelection)}`
+    }
+    if( game == 1 && computerSelection == 1 || game == 2 && computerSelection == 0 || game == 3 && computerSelection == 2){
+        return {
+            playerWin: false,
+            computerWin: true,
+            message: `Computer Wins! computer selected: ${intToString(computerSelection)} you selected: ${intToString(playerSelection)}`
+        }
+    }
+    else {
+        return {
+            playerWin: true,
+            computerWin: false,
+            message: `You Won! computer selected: ${intToString(computerSelection)} you selected: ${intToString(playerSelection)}`
+        }
+    }
+}
+
 function gameController(){
     computerChoice = computerPlay();
     userChoice = userPlay();
+    result = gameResult(computerChoice, userChoice);
+    console.log(result);
     // evaluate game play
     
 }
