0012ccc6cd35414700057cf9c3644b386e09665f
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-06 21:44:56 -0400

    fix math random and game result

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,17 +2,17 @@ function computerPlay(){
     // rock = 1
     // paper = 2
     // scissors = 3
-    return Math.random(Math.floor(Math.random() * 3) + 1);
+    return Math.floor((Math.random() * 3) + 1);
 }
 
 function userPlay(){
-    usershoice = prompt("Rock Paper Scissors Shoot! Type your choice: ");
+    userchoice = prompt("Rock Paper Scissors Shoot! Type your choice: ");
     if(userchoice == "rock"){ return 1;}
     if(userchoice == "paper"){return 2;}
     if(userchoice == "scissors"){return 3;}
 }
 
-function gamePlay(computerSelection, playerSelection){
+function gameResult(computerSelection, playerSelection){
     let game = computerSelection + playerSelection;
     // rock 1 paper 2 = 3 (paper wins)
     // rock 1 scissors 3 = 4 (rock wins)
@@ -20,20 +20,20 @@ function gamePlay(computerSelection, playerSelection){
     if (computerSelection == playerSelection) return {
         playerWin: true,
         computerWin: true,
-        message: `Its a Tie, both selected ${intToString(computerSelection)}`
+        message: `Its a Tie, both selected ${computerSelection}`
     }
     if( game == 1 && computerSelection == 1 || game == 2 && computerSelection == 0 || game == 3 && computerSelection == 2){
         return {
             playerWin: false,
             computerWin: true,
-            message: `Computer Wins! computer selected: ${intToString(computerSelection)} you selected: ${intToString(playerSelection)}`
+            message: `Computer Wins! computer selected: ${computerSelection} you selected: ${playerSelection}`
         }
     }
     else {
         return {
             playerWin: true,
             computerWin: false,
-            message: `You Won! computer selected: ${intToString(computerSelection)} you selected: ${intToString(playerSelection)}`
+            message: `You Won! computer selected: ${computerSelection} you selected: ${playerSelection}`
         }
     }
 }
@@ -41,8 +41,10 @@ function gamePlay(computerSelection, playerSelection){
 function gameController(){
     computerChoice = computerPlay();
     userChoice = userPlay();
+    console.log("comp: " + computerChoice + " user: " + userChoice)
     result = gameResult(computerChoice, userChoice);
     console.log(result);
     // evaluate game play
-    
 }
+
+gameController();
