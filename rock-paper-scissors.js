38b50ee0c78822c158b0e7594e40850aa6697f44
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-11-08 17:13:23 -0500

    added styling + eventlistener + results on html

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,18 +1,39 @@
-function computerPlay(){
+var userChoiceImg = document.getElementById("user-choice");
+var computerChoiceImg = document.getElementById("computer-choice");
+var resultsH2 = document.getElementById("results-h2")
+
+var userChoice = "";
+
+var rpsButtons = document.getElementsByClassName("far");
+Array.from(rpsButtons).forEach(rpsButton => {
+    rpsButton.addEventListener("click", function () {
+        userChoice = rpsButton.id;
+        console.log("Click: " + userChoice)
+        gameController()
+    })
+});
+
+function computerPlay() {
     // rock = 1
     // paper = 2
     // scissors = 3
     return Math.floor((Math.random() * 3) + 1);
 }
 
-function userPlay(){
+function userPlay(userchoice) {
     //userchoice = prompt("Rock Paper Scissors Shoot! Type your choice: ");
-    if(userchoice == "rock"){ return 1;}
-    if(userchoice == "paper"){return 2;}
-    if(userchoice == "scissors"){return 3;}
+    if (userchoice == "rock") return 1;
+    if (userchoice == "paper") return 2;
+    if (userchoice == "scissors") return 3;
 }
 
-function gameResult(computerSelection, playerSelection){
+function intToStringRps(rpsInt){
+    if(rpsInt == 1) return "Rock";
+    if(rpsInt == 2) return "Paper";
+    if(rpsInt == 3) return "Scissors";
+}
+
+function gameResult(computerSelection, playerSelection) {
     let game = computerSelection + playerSelection;
     // rock 1 paper 2 = 3 (paper wins)
     // rock 1 scissors 3 = 4 (rock wins)
@@ -20,31 +41,46 @@ function gameResult(computerSelection, playerSelection){
     if (computerSelection == playerSelection) return {
         playerWin: true,
         computerWin: true,
-        message: `Its a Tie, both selected ${computerSelection}`
+        message: `Its a Tie`
     }
-    if( game == 1 && computerSelection == 1 || game == 2 && computerSelection == 0 || game == 3 && computerSelection == 2){
+    if (game == 3 && computerSelection == 2 || game == 4 && computerSelection == 1 || game == 5 && computerSelection == 3) {
         return {
             playerWin: false,
             computerWin: true,
-            message: `Computer Wins! computer selected: ${computerSelection} you selected: ${playerSelection}`
+            message: `Computer Wins!`
         }
-    }
-    else {
+    } else {
         return {
             playerWin: true,
             computerWin: false,
-            message: `You Won! computer selected: ${computerSelection} you selected: ${playerSelection}`
+            message: `You Won!`
         }
     }
 }
 
-function gameController(){
-    computerChoice = computerPlay();
-    userChoice = userPlay();
-    console.log("comp: " + computerChoice + " user: " + userChoice)
-    result = gameResult(computerChoice, userChoice);
-    console.log(result);
-    // evaluate game play
+function populateResultIcons() {
+    console.log("populate: "+ "u: " + intToStringRps(userChoice) + " c: " + intToStringRps(computerChoice));
+    userChoiceImg.removeAttribute('class')
+    computerChoiceImg.removeAttribute('class')
+
+    userChoiceImg.classList.add("fas")
+    if (userChoice == 1) userChoiceImg.classList.add("fa-hand-rock")
+    if (userChoice == 2) userChoiceImg.classList.add("fa-hand-paper")
+    if (userChoice == 3) userChoiceImg.classList.add("fa-hand-scissors")
+    
+    computerChoiceImg.classList.add("fas");
+    if (computerChoice == 1) computerChoiceImg.classList.add("fa-hand-rock")
+    if (computerChoice == 2) computerChoiceImg.classList.add("fa-hand-paper")
+    if (computerChoice == 3) computerChoiceImg.classList.add("fa-hand-scissors")
 }
 
-gameController();
+function gameController() {
+    computerChoice = computerPlay();
+    userChoice = userPlay(userChoice);
+    console.log("user: " + intToStringRps(userChoice) + " comp: " + intToStringRps(computerChoice))
+    result = gameResult(computerChoice, userChoice);
+    resultsH2.innerText = result.message;
+    populateResultIcons();
+    // console.log(result);
+    // evaluate game play
+}
